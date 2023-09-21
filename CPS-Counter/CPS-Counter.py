b75960a7beed4834ad0752277bea9a4c6280d3df
Merlijnos <99071140@mydavinci.nl>
2023-09-21 09:51:55 +0200

    Moved CPS-Counter and Elden-Ring to Python-Projects

---
+++ CPS-Counter/CPS-Counter.py
@@ -0,0 +1,65 @@
+import time
+import keyboard
+import tkinter as tk
+from tkinter import font
+from PIL import Image, ImageTk
+
+class CPSCounter:
+    def __init__(self, duration):
+        self.duration = duration
+        self.clicks = 0
+        self.cps = 0
+        self.high_score = 0
+        self.is_running = False
+        self.start_time = 0
+        self.end_time = 0
+        self.root = tk.Tk()
+        self.root.title("CPS Counter")
+        self.root.geometry("300x200")
+        self.root.config(bg="#1E1E1E")
+        self.font = font.Font(family="Helvetica", size=12, weight="bold")
+        self.label = tk.Label(self.root, text="Press Start to begin", font=self.font, bg="#1E1E1E", fg="#FFFFFF")
+        self.label.pack(pady=20)
+        self.start_button_image = Image.open("start_button.png")
+        self.start_button_image = self.start_button_image.resize((100, 50), Image.BICUBIC)
+        self.start_button_photo = ImageTk.PhotoImage(self.start_button_image)
+        self.start_button = tk.Button(self.root, image=self.start_button_photo, bg="#1E1E1E", bd=0, command=self.start)
+        self.start_button.pack(pady=10)
+        self.high_score_label = tk.Label(self.root, text=f"High Score: {self.high_score:.2f}", font=self.font, bg="#1E1E1E", fg="#FFFFFF")
+        self.high_score_label.pack(pady=10)
+        self.root.mainloop()
+
+    def start(self):
+        self.is_running = True
+        self.start_time = time.time()
+        self.end_time = self.start_time + self.duration
+        self.clicks = 0
+        self.update_label()
+        self.start_button.config(state="disabled")
+        self.root.after(100, self.check_key_press)
+
+    def check_key_press(self):
+        if self.is_running and time.time() < self.end_time:
+            if keyboard.is_pressed('space') and not self.spacebar_pressed:
+                self.spacebar_pressed = True
+                self.clicks += 1
+            elif not keyboard.is_pressed('space'):
+                self.spacebar_pressed = False
+            self.root.after(10, self.check_key_press)
+        else:
+            self.is_running = False
+            self.cps = self.clicks / self.duration
+            self.update_label()
+            self.start_button.config(state="normal")
+
+    def update_label(self):
+        if self.is_running:
+            time_left = int(self.end_time - time.time())
+            milliseconds_left = int((self.end_time - time.time()) * 100) % 100
+            self.label.config(text=f"Time Left: {time_left:02d}.{milliseconds_left:02d} s\nClicks: {self.clicks}\nCPS: {self.cps:.2f}")
+            self.root.after(100, self.update_label)
+        else:
+            self.label.config(text=f"You clicked {self.clicks} times in {self.duration} seconds.\nYour clicks per second (CPS) is {self.cps:.2f}.")
+
+duration = 10
+cps_counter = CPSCounter(duration)  
---
+++ Elden-Ring/Elden-Ring.py
@@ -0,0 +1,99 @@
+import pygame
+import random
+
+pygame.init()
+
+# Set up the game window
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Elden Ring")
+
+# Load background music
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+# Load sound effect
+explosion_sound = pygame.mixer.Sound("explosion.wav")
+
+# Load player image
+player_image = pygame.image.load("player.png")
+player_width = player_image.get_width()
+player_height = player_image.get_height()
+player_image = pygame.transform.scale(player_image, (player_width // 2, player_height // 2))
+player_width = player_image.get_width()
+player_height = player_image.get_height()
+player_x = screen_width // 2 - player_width // 2
+player_y = screen_height - player_height - 10
+player_speed = 5
+
+# Load enemy image
+enemy_image = pygame.image.load("enemy.png")
+enemy_image = pygame.transform.rotate(enemy_image, -90)
+enemy_image = pygame.transform.scale(enemy_image, (64, 64))
+enemy_x = random.randint(0, screen_width - enemy_image.get_width())
+enemy_y = random.randint(-screen_height, -enemy_image.get_height())
+enemy_speed = 1
+
+# Load background image
+background_image = pygame.image.load("background.png")
+
+# Define score variable
+score = 0
+
+# Define game over variables
+game_over = False
+game_over_font = pygame.font.Font(None, 72)
+game_over_text = game_over_font.render("Game Over", True, (255, 255, 255))
+game_over_rect = game_over_text.get_rect(center=(screen_width // 2, screen_height // 2))
+
+# Game loop
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update game state
+    if not game_over:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player_x > 0:
+            player_x -= player_speed
+        if keys[pygame.K_RIGHT] and player_x < screen_width - player_width:
+            player_x += player_speed
+
+        enemy_y += enemy_speed + score // 10
+        if enemy_y > screen_height:
+            enemy_x = random.randint(0, screen_width - enemy_image.get_width())
+            enemy_y = random.randint(-screen_height, -enemy_image.get_height())
+            score += 1
+
+        # Check for collisions
+        if player_image.get_rect(topleft=(player_x, player_y)).colliderect(enemy_image.get_rect(topleft=(enemy_x, enemy_y))):
+            explosion_sound.play()
+            game_over = True
+
+            # Stop background music
+            pygame.mixer.music.stop()
+
+    # Draw game objects
+    screen.blit(background_image, (0, 0))
+    screen.blit(player_image, (player_x, player_y))
+    screen.blit(enemy_image, (enemy_x, enemy_y))
+
+    # Draw score
+    font = pygame.font.Font(None, 36)
+    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+    # Draw game over screen
+    if game_over:
+        screen.blit(game_over_text, game_over_rect)
+
+    # Update display
+    pygame.display.update()
+
+# Quit Pygame
+pygame.quit()
